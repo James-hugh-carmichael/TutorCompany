e94361ce9932b8cc5d2deb5ca38776b88dd5c8ec
James-Hugh-Carmichael <carmichaeljames06@gmail.com>
2024-09-12 10:55:14 +0100

    Added menu bar for smaller screens, design sorted

--- Page1/script.js
+++ Page1/script.js
@@ -6,6 +6,12 @@ let button1 = document.getElementById("button1");
 let button2 = document.getElementById("button2");
 let button3 = document.getElementById("button3");
 
+let navTitle = document.getElementById("navTitle");
+let hamburger = document.getElementById("hamburger");
+let navLinks = document.getElementById("navLinks");
+let exitHamburger = document.getElementById("exitHamburger");
+
+
 function hideAllImagesAndButtons() {
     photo1.style.display = 'none';
     photo2.style.display = 'none';
@@ -13,7 +19,6 @@ function hideAllImagesAndButtons() {
     button1.style.backgroundColor = 'buttonface';
     button2.style.backgroundColor = 'buttonface';
     button3.style.backgroundColor = 'buttonface';
-
 }
 
 button1.onclick = function() {
@@ -37,3 +42,30 @@ button3.onclick = function() {
 hideAllImagesAndButtons();
 photo1.style.display = 'block';
 button1.style.backgroundColor = 'grey';
+
+hamburger.onclick = function() {
+    navLinks.classList.add("show"); // Slide in the menu
+    hamburger.style.display = 'none';
+    exitHamburger.style.display = 'inline';
+    navTitle.style.display = 'inline';
+};
+
+exitHamburger.onclick = function() {
+    navLinks.classList.remove("show"); // Slide out the menu
+    hamburger.style.display = 'block';
+    exitHamburger.style.display = 'none';
+    navTitle.style.display = 'none';
+};
+
+window.addEventListener('resize', function() {
+    if (window.innerWidth >= 1200) {
+        navLinks.classList.remove('show');  
+        hamburger.style.display = 'none';   
+        exitHamburger.style.display = 'none';
+        navTitle.style.display = 'none';
+    } else {
+        hamburger.style.display = 'block';  
+    }
+});
+
+
